7dfb1a45678e16e96963d88c49fce19b85eba2a7
Justice Ogbonna <ebesco@yahoo.com>
2017-04-11 21:51:50 -0400

    import super tabs to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SuperTabsModule } from '../super-tabs';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    SuperTabsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
